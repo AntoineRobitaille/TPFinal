58a8c2fbada00011726e0bc604c2cd04c563e99b
eleves <you@yourdomain.com>
2017-07-20 15:33:02 -0400

    UserDefault a faire

---
+++ ARProjetFinal/Singleton.swift
@@ -0,0 +1,13 @@
+//=========================
+import Foundation
+//=========================
+
+class Singleton{
+    var unArray = [String]()
+    static let instancePartage = Singleton()
+}
+//=========================
+
+class ListeDesTaches{
+    
+}
--- ARProjetFinal/ViewController.swift
+++ ARProjetFinal/ViewController.swift
@@ -2,27 +2,59 @@
 import UIKit
 //=====================================
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
-//-------------------------
+
+
+    @IBOutlet weak var tableV: UITableView!
+    @IBOutlet weak var newElement: UITextField!
+    
+    var viewContent: [String] = []
+    var defaults = UserDefaults.standard
+    
+    //-------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        loadData()
     }
-//-------------------------
+    //-------------------------
+    func loadData() {
+        viewContent = []
+        for a in Singleton.instancePartage.unArray{
+            viewContent.append(a)
+        }
+    }
+    //-------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-//-------------------------
-    @IBOutlet weak var tableV: UITableView!
+    //-------------------------
+    @IBAction func Ajouter(_ sender: UIButton) {
+        
+        if(newElement.text?.isEmpty)!{
+            let alerte = UIAlertController(title: "Champ vide", message:"Veuillez entrer une tâche pour l'ajouter à la liste", preferredStyle: UIAlertControllerStyle.alert)
+            alerte.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            
+            self.present(alerte,animated: true, completion: nil)
+        }
+        else{
+            print("message enregistrer")
+            Singleton.instancePartage.unArray.append(newElement.text!)
+            
+            loadData()
+            tableV.reloadData()
+        }
+    }
     
-
-    //---------------------
+    
+    //----Methodes pour table view-----------------
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         tableView.backgroundColor = UIColor.clear
-        return 20
+        return viewContent.count
     }
     //---------------------
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier:"proto")
-        cell.textLabel!.text = "Hello World"
+        cell.textLabel!.text = viewContent[indexPath.row]
         cell.textLabel?.textColor = UIColor.white
         cell.backgroundColor = UIColor.clear
         return cell
@@ -39,7 +71,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     //---------------------
-    
     
 }
 
