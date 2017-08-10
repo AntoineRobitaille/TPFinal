63a6872b3378ae4324bc3f3c1f4f568636c68b21
eleves <you@yourdomain.com>
2017-08-10 14:43:02 -0400

    1/4 bouton fonctionne (reinitialiser)

--- ARProjetFinal/ViewController.swift
+++ ARProjetFinal/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tableV.allowsMultipleSelection = true
         loadData()
     }
     //-------------------------
@@ -27,7 +28,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    //-------------------------
+    //---Bouton ajouter
     @IBAction func Ajouter(_ sender: UIButton) {
         
         if(newElement.text?.isEmpty)!{
@@ -37,21 +38,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             self.present(alerte,animated: true, completion: nil)
         }
         else{
-            print("message enregistrer")
             Singleton.instancePartage.unArray.append(newElement.text!)
             
             loadData()
             tableV.reloadData()
         }
+        newElement.text = ""
+    }
+    //---Bouton Sauvegarder
+    //sauvegarder toutes les infos sur le serveur
+    @IBAction func Sauvegarder(_ sender: UIButton) {
+        
+    }
+    
+    //---Bouton réinitialiser
+    //déselectionner toutes les taches sélectionnés
+    @IBAction func Reinitialiser(_ sender: UIButton) {
+        tableV.reloadData()
+    }
+   
+    //---Bouton charger
+    //va chercher les informations sauvegarder sur le serveur et update la liste
+    @IBAction func Charger(_ sender: UIButton) {
+        
+    }
+    //---Bouton Voir Liste
+    //Prend les elements sélectionné et les envoies dans la liste
+    @IBAction func VoirListe(_ sender: UIButton) {
+        
     }
     
     
     //----Methodes pour table view-----------------
+    //----Nombre de cellules
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         tableView.backgroundColor = UIColor.clear
         return viewContent.count
     }
     //---------------------
+    //----Texte dans les cellules
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier:"proto")
         cell.textLabel!.text = viewContent[indexPath.row]
@@ -60,11 +85,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     //---------------------
+    //----Selection des cellules
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath as IndexPath)!
         selectedCell.contentView.backgroundColor = UIColor.darkGray
     }
     //---------------------
+    //----Éliminer les cellules
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCellEditingStyle.delete {
             tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
